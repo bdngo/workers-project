d63319b5c7b7a00c8eaf9fbe2df0112f29189dcb
Bryan Ngo <bryanngo@berkeley.edu>
2021-12-21 02:50:28 -0800

    remove test comments

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,4 @@ import { handleRequest } from './handler'
 
 addEventListener('fetch', (event: FetchEvent) => {
   event.respondWith(handleRequest(event.request))
-  // const post = { title: 'frick', username: 'bryan', content: 'hello there' }
-  // handleRequest(
-  //   new Request('/', { method: 'POST', body: JSON.stringify(post) }),
-  // )
-
-  // event.respondWith(handleRequest(new Request('/', { method: 'GET' })))
 })
