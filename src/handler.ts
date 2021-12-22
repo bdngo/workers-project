99e8df22d72861cf842aa3471d5558986bb61c5f
Bryan Ngo <bryanngo@berkeley.edu>
2021-12-22 12:05:23 -0800

    run linter

--- src/handler.ts
+++ src/handler.ts
@@ -4,14 +4,16 @@ export async function handleRequest(request: Request): Promise<Response> {
     return new Response('Invalid endpoint', { status: 503 })
   }
   switch (request.method) {
-    case 'GET':
+    case 'GET': {
       const getResp = await handleGet()
       getResp.headers.set('Access-Control-Allow-Origin', '*')
       return getResp
-    case 'POST':
+    }
+    case 'POST': {
       const postResp = await handlePost(request)
       postResp.headers.set('Access-Control-Allow-Origin', '*')
       return postResp
+    }
     default:
       return new Response('Invalid request', { status: 501 })
   }
@@ -20,7 +22,7 @@ export async function handleRequest(request: Request): Promise<Response> {
 async function handleGet(): Promise<Response> {
   const keys_lst = await WP_NSPACE_PROD.list()
   const keys = keys_lst.keys
-  let posts: string[] = []
+  const posts = []
   for (const elem of keys) {
     const val: string | null = await WP_NSPACE_PROD.get(elem.name)
     if (val === null) {
@@ -33,7 +35,7 @@ async function handleGet(): Promise<Response> {
 }
 
 async function handlePost(request: Request): Promise<Response> {
-  const jsObj: any = await request.json()
+  const jsObj = await request.json()
   await WP_NSPACE_PROD.put(
     Math.random().toString(16).substring(2, 8),
     JSON.stringify(jsObj),
